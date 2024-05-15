2a6621c643c65ab8e085596e35f323d8abbe41cd
AGLINFO\dam117 <jgarciap11@educantabria.es>
2024-05-15 11:58:43 +0200

    finalizado

--- src/retofinal/PanelPrincipalApp.java
+++ src/retofinal/PanelPrincipalApp.java
@@ -179,12 +179,11 @@ public class PanelPrincipalApp extends javax.swing.JFrame {
     private void jButtonAceptarSesionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonAceptarSesionActionPerformed
         try {
             int nivel = jComboBoxNivel.getSelectedIndex();
-            
+            System.out.println(nivel);
             nombres = jTextFieldAliasSesion.getText();
             level = jComboBoxNivel.getSelectedItem()+"";
-
             xml(nombres, level);
-            System.out.println(nivel);
+            
             PuertaFacil pFacil = new PuertaFacil();
 
             PuertaMedio pMedio = new PuertaMedio();
