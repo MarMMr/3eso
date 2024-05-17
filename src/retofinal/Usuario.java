1cb879d01524710deedd159462f523efaf285a02
MarMMr <Usuario@DESKTOP-J33M9KF>
2024-05-17 10:12:15 +0200

    Viernes

--- src/retofinal/Usuario.java
+++ src/retofinal/Usuario.java
@@ -9,5 +9,22 @@ package retofinal;
  * @author dam117
  */
 public class Usuario {
-    
+    protected String alias;
+    protected String nombre;
+    protected String email;
+    protected int edad;
+    protected int puntuacion;
+
+    public Usuario(String alias, String nombre, String email, int edad) {
+        this.alias = alias;
+        this.nombre = nombre;
+        this.email = email;
+        this.edad = edad;
+        //this.puntuacion=puntuacion;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "alias=" + alias + ", nombre=" + nombre + ", email=" + email + ", edad=" + edad + ", puntuacion= "+puntuacion+'}';
+    }
 }
